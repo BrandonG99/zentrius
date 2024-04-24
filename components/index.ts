4a3233c6bd5d7b8625574f15a56e4721169644ee
BrandonG99 <87080239+BrandonG99@users.noreply.github.com>
2024-04-24 15:29:13 +0100

    chore: add components/index.ts

---
+++ components/index.ts
@@ -0,0 +1,29 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CustomButton from './CustomButton';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
+import CarModal from './CarModal';
+import ShowMore from './ShowMore';
+import ImageSlideshow from './ImageSlideshow';
+import SignUp from './SignUp';
+import Login from './Login';
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomButton,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarModal,
+  ShowMore,
+  ImageSlideshow,
+  SignUp, 
+  Login
+}
